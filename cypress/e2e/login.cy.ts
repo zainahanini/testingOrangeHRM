8c9894bb8eb6c1a2a084a08d780ac67208a5106c
ZAINA <zainahanini66@gmail.com>
2025-09-09 21:04:15 +0300

    add Cypress tests for OrangeHRM login functionality

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,74 @@
+describe("OrangeHRM Login", () => {
+    beforeEach(() => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    
+    });
+
+    it("Should log in successfully with valid username and password", ()    => {
+        cy.get("input[name='username']").type("Admin");
+        cy.get("input[name='password']").type("admin123");
+        cy.get("button[type='submit']").click();
+        cy.contains("Dashboard").should("be.visible");
+    })
+
+    it("Should show error for valid username and invalid password", () => {
+        cy.get("input[name='username']").type("Admin");
+        cy.get("input[name='password']").type("wrongpass");
+        cy.get("button[type='submit']").click();
+        cy.contains("Invalid credentials").should("be.visible");
+    })
+
+    it("Should show error for invalid username and valid password", () => {
+         cy.get("input[name='username']").type("WrongUser");
+        cy.get("input[name='password']").type("admin123");
+        cy.get("button[type='submit']").click();
+        cy.contains("Invalid credentials").should("be.visible");
+    })
+
+    it("Should show error for invalid username and invalid password", () => {
+         cy.get("input[name='username']").type("WrongUser");
+        cy.get("input[name='password']").type("wrongpass");
+        cy.get("button[type='submit']").click();
+        cy.contains("Invalid credentials").should("be.visible");
+    })
+
+    it("Should show required message when username is empty and password is valid", () => {
+         cy.get("input[name='username']").type(" ");
+        cy.get("input[name='password']").type("admin123");
+        cy.get("button[type='submit']").click();
+        cy.contains("Required").should("be.visible");
+    })
+
+    it("Should show required message when username is empty and password is invalid", () => {
+        cy.get("input[name='username']").type(" ");
+        cy.get("input[name='password']").type("wrongpass");
+        cy.get("button[type='submit']").click();
+        cy.contains("Required").should("be.visible");
+    })
+
+    it("Should show required message when password is empty and username is valid", () =>{
+        cy.get("input[name='username']").type("Admin");
+        cy.get("input[name='password']").type(" ");
+        cy.get("button[type='submit']").click();
+        cy.contains("Required").should("be.visible");
+    })
+
+    it("Should show required message when password is empty and username is invalid", () =>{
+        cy.get("input[name='username']").type("WrongUser");
+        cy.get("input[name='password']").type(" ")
+        cy.get("button[type='submit']").click();
+        cy.contains("Required").should("be.visible");
+
+    })
+
+    it("Should show required messages when both username and password are empty", () =>{
+        cy.get("input[name='username']").type(" ");
+        cy.get("input[name='password']").type(" ")
+        cy.get("button[type='submit']").click();
+        cy.contains("Required").should("be.visible");
+    })
+
+    it("Should mask password input by default", () => {
+        cy.get("input[name='password']").should("have.prop", "type", "password");
+    })
+});
